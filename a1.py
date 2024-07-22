32c3a1823e433412be9cee8cb603ddd2b3a2033f
03rakesh <harisivaraman30@gmail.com>
2024-07-22 16:46:58 +0530

    fourth program

--- a1.py
+++ a1.py
@@ -11,3 +11,10 @@ print(e)
 print('hello')
 d1={'name':'rakesh','age':29,'add':'blr'}
 print(d1)
+a=Input("enter a value="a)
+b=input("enter a value="b)
+c=a
+a=b
+b=c
+print("a=" +a)
+print("b=" +b)
